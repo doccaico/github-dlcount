4dacfbf6c1975534acd0607af3285eaca2502cbe
doccaico <hash_ext_luna@yahoo.co.jp>
2025-08-05 07:38:15 +0900

    up

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-GOOS=linux GOARCH=amd64 go build -o github_dlcount_linux -ldflags="-s -w" -trimpath ./cmd/github_dlcount
+GOOS=linux GOARCH=amd64 go build -o github_dlcount_linux -ldflags="-s -w" -trimpath ./cmd/github-dlcount
 
-GOOS=windows GOARCH=amd64 go build -o github_dlcount_windows.exe -ldflags="-s -w" -trimpath ./cmd/github_dlcount
+GOOS=windows GOARCH=amd64 go build -o github_dlcount_windows.exe -ldflags="-s -w" -trimpath ./cmd/github-dlcount
